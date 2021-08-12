48eda2d29b423992fcb1fb96361a805f52cbea05
mobius-crypt <mobiusndou@gmail.com>
2021-08-12 12:12:30 +0200

    updated: added scheduler

--- _cron/scheduler.py
+++ _cron/scheduler.py
@@ -1,17 +1,22 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+import asyncio
 
 schedule = BackgroundScheduler()
 
 
-@schedule.scheduled_job('interval', seconds=3)
-def timed_job():
+@schedule.scheduled_job('interval', minutes=5)
+def task_run():
     ***REMOVED***
         launch processes here
     :return:
     ***REMOVED***
-    print("this jub run every 3 minutes")
+    print("this job run every 5 minutes")
 
 
 @schedule.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
+    ***REMOVED***
+
+    :return:
+    ***REMOVED***
     print('This job is run every weekday at 5pm.')
