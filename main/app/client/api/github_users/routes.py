bf51f6c08db2a2315ac914b12b23f7cbefb991c4
mobius-crypt <mobiusndou@gmail.com>
2021-08-13 13:39:04 +0200

    added: github users api

---
+++ main/app/client/api/github_users/routes.py
@@ -0,0 +1,19 @@
+***REMOVED***
+    **github_users module**
+        admin routes for github authentication users data
+
+
+***REMOVED***
+from flask import Blueprint
+
+github_users_api_bp = Blueprint('github_users_api', __name__)
+
+
+@github_users_api_bp.route('/_api/client/github-users/', methods=['POST', 'GET'])
+def github_users_api() -> tuple:
+    ***REMOVED***
+        **github users client api**
+
+    :return:
+    ***REMOVED***
+    pass
