f198a322e7d369fddaace7e854058d3068418499
mobius-crypt <mobiusndou@gmail.com>
2021-08-14 14:19:59 +0200

    updated: users admin api
    included authentication

--- main/app/admin/api/users/users.py
+++ main/app/admin/api/users/users.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
+from config.exceptions import error_codes, UnAuthenticatedError
 from views.users import UserView
 from typing import Optional
 
@@ -8,14 +9,18 @@ admin_users_api_bp = Blueprint("admin_users_api", __name__)
 @admin_users_api_bp.route('/_api/admin/users/<string:path>', methods=["GET", "POST"])
 def admin_users(path: str) -> tuple:
     ***REMOVED***
-        # TODO decide on the authentication mechanism for admin section
     :param  path: indicates what route to follow for admin users
     :return:
     ***REMOVED***
     # NOTE: only this application can call this endpoints-
     # For Purposes of authentication check if the calling url belongs to this app
+    json_data: dict = request.get_json()
+    secret_key: Optional[str] = json_data.get("secret_key")
+    if not isinstance(secret_key, str) or secret_key != current_app.config.get('SECRET_KEY'):
+        message: str = 'User Not Authorized: you cannot perform this action'
+        raise UnAuthenticatedError(status=error_codes.access_forbidden_error_code, description=message)
+
     if path == "get":
-        json_data: dict = request.get_json()
         organization_id: Optional[str] = json_data.get("organization_id")
         uid: Optional[str] = json_data.get("uid")
         user_view_instance: UserView = UserView()
@@ -23,8 +28,6 @@ def admin_users(path: str) -> tuple:
 
     elif path == "is-user-unique":
         # checks if user exists based on either email or uid - used for administration purposes
-        json_data: dict = request.get_json()
-        print('JSON DAT: ', json_data)
         email: Optional[str] = json_data.get("email")
         uid: Optional[str] = json_data.get("uid")
         user_view_instance: UserView = UserView()
