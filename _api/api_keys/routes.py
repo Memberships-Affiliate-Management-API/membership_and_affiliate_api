3942e785b45338270884c362ce8650eea02f3ff0
mobius-crypt <mobiusndou@gmail.com>
2021-08-14 12:44:30 +0200

    feature: added deactivate_key route

--- _api/api_keys/routes.py
+++ _api/api_keys/routes.py
@@ -41,6 +41,28 @@ def create_client_api_key() -> tuple:
     domain: Optional[str] = json_data.get('domain')
     uid: Optional[str] = json_data.get('uid')
     organization_id: Optional[str] = json_data.get('organization_id')
+    secret_key: Optional[str] = json_data.get('SECRET_KEY')
+    if isinstance(secret_key, str) and secret_key == current_app.config.get('SECRET_KEY'):
+        api_view_instance: APIKeysView = APIKeysView()
+        return api_view_instance.create_keys(domain=domain, uid=uid, organization_id=organization_id)
 
-    api_view_instance: APIKeysView = APIKeysView()
-    return api_view_instance.create_keys(domain=domain, uid=uid, organization_id=organization_id)
+    message: str = "User not Authorized: you are not authorized to call this API"
+    raise UnAuthenticatedError(status=error_codes.un_auth_error_code, description=message)
+
+
+@api_keys_bp.route('/_api/client/keys/deactivate', methods=['POST'])
+def deactivate_key() -> tuple:
+    ***REMOVED***
+
+    :return:
+    ***REMOVED***
+    json_data: dict = request.get_json()
+    api_key: Optional[str] = json_data.get('api-key')
+    secret_key: Optional[str] = json_data.get('SECRET_KEY')
+    if isinstance(secret_key, str) and secret_key == current_app.config.get('SECRET_KEY'):
+        api_view_instance: APIKeysView = APIKeysView()
+        return api_view_instance.deactivate_key(key=api_key)
+
+    message: str = "User not Authorized: you are not authorized to call this API"
+    raise UnAuthenticatedError(status=error_codes.un_auth_error_code, description=message)
+
