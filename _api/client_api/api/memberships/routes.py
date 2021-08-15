c407db4cbb2cab3c2a1bf29f247895a1e8faca28
mobius-crypt <mobiusndou@gmail.com>
2021-08-15 14:57:06 +0200

    updated: client memberships management routes

--- _api/client_api/api/memberships/routes.py
+++ _api/client_api/api/memberships/routes.py
@@ -9,9 +9,7 @@ __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affi
 __github_profile__ = "https://github.com/freelancing-solutions/"
 
 from typing import Optional
-
 from flask import Blueprint, request, current_app
-
 from config.exceptions import if_bad_request_raise, UnAuthenticatedError, error_codes
 
 memberships_client_api_bp = Blueprint('memberships_client_api', __name__)
@@ -22,9 +20,10 @@ def memberships_client_api(path: str) -> tuple:
     ***REMOVED***
         **memberships_client_api**
             handles client admin requests for memberships -
-            this means clients or developers will subscribe to membership plans
-            in order to use this website from this endpoint
+            this means clients or developers
 
+            **USE CASES**
+                1. will subscribe to membership plans in order to use this website from this endpoint
             - this endpoints can be called from the client dashboard
             in order to start using the API for as a paid client or a free client
     :param path:
@@ -38,9 +37,29 @@ def memberships_client_api(path: str) -> tuple:
 
         if not isinstance(secret_key, str) or secret_key != current_app.config.get('SECRET_KEY'):
             message: str = 'User Not Authorized: you cannot perform this action'
-            raise UnAuthenticatedError(status=error_codes.access_forbidden_error_code, description=message)
+            raise UnAuthenticatedError(status=error_codes.access_forbidden_error_code,
+                                       description=message)
 
         elif path == "join-plan":
             pass
         elif path == "leave-plan":
             pass
+
+    return "OK", 200
+
+
+@memberships_client_api_bp.route('_api/v1/client/admin/memberships/<string:path>', methods=["POST"])
+def client_memberships_management(path: str) -> tuple:
+    ***REMOVED***
+        **client_memberships_management**
+            used to manage client's membership plans which they are offering to the
+            public - this api will also be called from client dashboard app
+
+            **USE CASES**
+                1. create and update services or products
+                2. create and update membership plans / payment plans for services or products
+
+    :param path:
+    :return:
+    ***REMOVED***
+    pass
