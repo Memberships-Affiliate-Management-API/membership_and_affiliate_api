de133071230f1926ecaef055c1e239ba7ed776d4
mobius-crypt <mobiusndou@gmail.com>
2021-08-15 16:41:29 +0200

    updated: added organizations list

--- main/app/client/routes/dashboard.py
+++ main/app/client/routes/dashboard.py
@@ -1,26 +1,52 @@
 ***REMOVED***
     this is a private route
 ***REMOVED***
+from typing import List
+
 from flask import Blueprint, request, render_template, url_for, get_flashed_messages, redirect, flash
 
 from config.exceptions import status_codes
 from database.users import UserModel
 from main import app_cache
 from security.users_authenticator import handle_users_auth
-from utils.utils import return_ttl, can_cache
+from utils.utils import return_ttl, can_cache, create_id
+
 client_dashboard_bp = Blueprint('client_dashboard', __name__)
+
+
+temp_organizations: List[dict] = [
+    {
+        'organization_id': create_id(),
+        'organization_name': 'Memberships API',
+        'description': 'if you are interested in our api tools you are welcome'
+
+    },
+    {
+        'organization_id': create_id(),
+        'organization_name': 'Memberships APIKeysView & Memberships',
+        'description': 'if you are interested in our api tools you are welcome'
+    }
+]
 
 
 @client_dashboard_bp.route('/client/dashboard', methods=["GET"])
 @handle_users_auth
 def client_dashboard(current_user: UserModel) -> tuple:
+    ***REMOVED***
+        **client_dashboard**
+            allows client to get access to their organizations
+    :param current_user:
+    :return:
+    ***REMOVED***
     get_flashed_messages()
     if not(current_user and current_user.uid):
         # TODO: insure local cache does not cache info if redirected
         flash('Please login or register to start using this app')
         return redirect(url_for('memberships_main.memberships_main_routes', path='login'))
 
-    return render_template('client/dashboard.html', current_user=current_user), status_codes.status_ok_code
+    # TODO - retrive a list of organizations here
+    return render_template('client/dashboard.html', current_user=current_user,
+                           organizations_list=temp_organizations), status_codes.status_ok_code
 
 
 @client_dashboard_bp.route('/client/dashboard/<path:path>', methods=["GET"])
@@ -32,7 +58,8 @@ def client_dashboard_routes(current_user: UserModel, path: str) -> tuple:
         return redirect(url_for('memberships_main.memberships_main_routes', path='login'))
 
     if path == "dashboard":
-        return render_template('client/dashboard.html', current_user=current_user), status_codes.status_ok_code
+        return render_template('client/dashboard.html', current_user=current_user,
+                               organizations_list=temp_organizations), status_codes.status_ok_code
     elif path == "organizations":
         return render_template('client/organizations.html', current_user=current_user), status_codes.status_ok_code
     elif path == "affiliates":
