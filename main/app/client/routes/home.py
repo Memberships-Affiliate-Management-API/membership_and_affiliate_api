f332ce6fb790f476b4cd16ea3f296df14f6962fa
mobius-crypt <mobiusndou@gmail.com>
2021-08-16 15:17:05 +0200

    updated: docstrings

--- main/app/client/routes/home.py
+++ main/app/client/routes/home.py
@@ -1,6 +1,13 @@
 ***REMOVED***
-    this is a public route
+    **Client Home Route**
+        entry page to client dashboard
 ***REMOVED***
+__author__ = "mobius-crypt"
+__email__ = "mobiusndou@gmail.com"
+__twitter__ = "@blueitserver"
+__github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
+__github_profile__ = "https://github.com/freelancing-solutions/"
+
 from flask import Blueprint, render_template, get_flashed_messages
 from config.exceptions import status_codes
 from database.users import UserModel
@@ -15,6 +22,12 @@ client_home_bp = Blueprint('client_home', __name__)
 @logged_user
 @app_cache.cached(timeout=return_ttl('short'))
 def client_home(current_user: UserModel) -> tuple:
+    ***REMOVED***
+        **client_home**
+            entry page to client dashboard
+    :param current_user:
+    :return:
+    ***REMOVED***
     get_flashed_messages()
     if current_user and current_user.uid:
         return render_template('client/home.html', current_user=current_user), status_codes.status_ok_code
