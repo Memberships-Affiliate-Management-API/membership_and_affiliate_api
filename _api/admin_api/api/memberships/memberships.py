6c3a714122636b70f72622e87cb6fb050bc4a812
mobius-crypt <mobiusndou@gmail.com>
2021-08-17 09:19:10 +0200

    refactored:  memberships.py

--- _api/admin_api/api/memberships/memberships.py
+++ _api/admin_api/api/memberships/memberships.py
@@ -1,23 +1,28 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, current_app
+
+from config.exceptions import UnAuthenticatedError, error_codes
 from views.memberships import MembershipPlansView, MembershipsView
 from security.users_authenticator import logged_user, is_app_admin, handle_users_auth
-from typing import Union
-
+from typing import Union, Optional
 
 membership_plans_admin_api_bp = Blueprint('memberships_admin_api', __name__)
 
 
 @membership_plans_admin_api_bp.route('/_api/v1/admin/membership-plans/<string:path>', methods=['GET', 'POST'])
-@handle_users_auth
-def memberships_admin_api(current_user, path: str) -> tuple:
+def memberships_admin_api(path: str) -> tuple:
     ***REMOVED***
         this endpoint is for purposes of user administration only not for admin purposes
+    :param current_user:
     :param path:
     :return:
     ***REMOVED***
+    json_data: dict = request.get_json()
+    secret_key: Optional[str] = json_data.get('SECRET_KEY')
+    if not isinstance(secret_key, str) or secret_key != current_app.config.get('SECRET_KEY'):
+        message: str = 'User Not Authorized: you cannot perform this action'
+        raise UnAuthenticatedError(status=error_codes.access_forbidden_error_code, description=message)
 
     if path == "get":
-        json_data: dict = request.get_json()
         organization_id: Union[str, None] = json_data.get('organization_id')
         uid: Union[str, None] = json_data.get('uid')
         membership_plan_view_instance: MembershipPlansView = MembershipPlansView()
