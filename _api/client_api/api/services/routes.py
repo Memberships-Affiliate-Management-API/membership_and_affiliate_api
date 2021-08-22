fe99a26d63afa0133420d571dfa59a98bd6d0ed8
mobius-crypt <mobiusndou@gmail.com>
2021-08-22 09:21:13 +0200

    updated:updating service update api

--- _api/client_api/api/services/routes.py
+++ _api/client_api/api/services/routes.py
@@ -28,25 +28,35 @@ def services_api(path: str) -> tuple:
     services_data: dict = request.get_json()
     secret_key: Optional[str] = services_data.get('SECRET_KEY')
     services_view_instance: ServicesView = ServicesView()
+
     if not isinstance(secret_key, str) or secret_key != current_app.config.get('SECRET_KEY'):
         message: str = 'User Not Authorized: you cannot perform this action'
         raise UnAuthenticatedError(status=error_codes.access_forbidden_error_code, description=message)
 
+    organization_id: Optional[str] = services_data.get('organization_id')
+    uid: Optional[str] = services_data.get('uid')
+    name: Optional[str] = services_data.get('name')
+    description: Optional[str] = services_data.get('description')
+    category: Optional[str] = services_data.get('category')
+    image_url: Optional[str] = services_data.get('image_url')
+    home_url: Optional[str] = services_data.get('home_url')
+
     if path == "create-service":
-        organization_id: Optional[str] = services_data.get('organization_id')
-        uid: Optional[str] = services_data.get('uid')
-        name: Optional[str] = services_data.get('name')
-        description: Optional[str] = services_data.get('description')
-        category: Optional[str] = services_data.get('category')
-        image_url: Optional[str] = services_data.get('image_url')
-        home_url: Optional[str] = services_data.get('home_url')
 
         return services_view_instance.create_service(organization_id=organization_id, uid=uid, name=name,
                                                      description=description, category=category, image_url=image_url,
                                                      home_url=home_url)
 
     elif path == "update-service":
-        pass
+        if_bad_request_raise(request)
+        services_id: Optional[str] = services_data.get('services_id')
+
+        return services_view_instance.update_service(services_id=services_id, organization_id=organization_id, uid=uid,
+                                                     name=name, description=description, category=category,
+                                                     image_url=image_url, home_url=home_url)
+
+
+
 
 
 
