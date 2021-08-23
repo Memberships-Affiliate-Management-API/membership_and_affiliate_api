130208ad9d233f7e508a528c9b9fb31ffb8adc26
mobius-crypt <mobiusndou@gmail.com>
2021-08-23 20:27:37 +0200

    refactored: scheduler

--- _cron/scheduler.py
+++ _cron/scheduler.py
@@ -1,27 +1,2 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from config import config_instance
-import asyncio
 schedule = BackgroundScheduler()
-
-
-@schedule.scheduled_job('interval', minutes=1)
-def send_emails():
-    ***REMOVED***
-        **send_emails**
-        this task sends emails
-
-
-    :return:
-    ***REMOVED***
-    print("this job run every 5 minutes")
-
-
-@schedule.scheduled_job('cron', day_of_week='mon-sun', hour=17)
-def daily_task_run():
-    ***REMOVED***
-        **daily_task_run**
-            launch daily processes here asynchronously
-
-    :return:
-    ***REMOVED***
-    print('This job is run every weekday at 5pm.')
