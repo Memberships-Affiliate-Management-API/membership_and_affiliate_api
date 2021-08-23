293279d5074166c4bcf2f2ec25326a650b0a6921
mobius-crypt <mobiusndou@gmail.com>
2021-08-23 20:29:15 +0200

    refactored: scheduler docs

--- _cron/scheduler.py
+++ _cron/scheduler.py
@@ -1,2 +1,8 @@
+***REMOVED***
+    **scheduler**
+        used to dynamically add jobs on a separate thread to complete tasks that should not interfere
+        with requests, or requests that takes a long time to complete
+***REMOVED***
+
 from apscheduler.schedulers.background import BackgroundScheduler
 schedule = BackgroundScheduler()
