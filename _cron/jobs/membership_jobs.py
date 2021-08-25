8d1eca8e69fee200df7308167ff52dc8591e447b
mobius-crypt <mobiusndou@gmail.com>
2021-08-25 09:08:43 +0200

    updated: added memberships cron jobs

---
+++ _cron/jobs/membership_jobs.py
@@ -0,0 +1,18 @@
+class MembershipsJobsTask:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        ***REMOVED***
+
+        :return:
+        ***REMOVED***
+        pass
+
+    def check_membership_payment_status(self):
+        ***REMOVED***
+
+        :return:
+        ***REMOVED***
+        pass
