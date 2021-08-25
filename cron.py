db01e5c8e683ce2bab0699fe28c032f1b03931a5
mobius-crypt <mobiusndou@gmail.com>
2021-08-25 15:28:15 +0200

    refactored: cron scheduler

--- cron.py
+++ cron.py
@@ -1,10 +1,32 @@
 ***REMOVED***
     will start cron scheduler as a service
 ***REMOVED***
+import asyncio
+from typing import Optional
+import aiohttp
 from config import config_instance
 from _cron.scheduler import cron_scheduler
 from config.exceptions import status_codes
-import requests
+
+
+async def __async_request(_url, json_data, headers) -> Optional[dict]:
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=_url, json=json_data, headers=headers) as response:
+            response, status = response
+            json_data = response.json()
+            if json_data.get('status'):
+                user_data: dict = json_data.get('payload')
+                return user_data
+            return None
+
+
+def send_request(_endpoint: str) -> None:
+    _base_url: str = config_instance.BASE_URL
+    _url: str = f"{_base_url}{_endpoint}"
+    organization_id: str = config_instance.ORGANIZATION_ID
+    headers: dict = {'content-type': 'application/json'}
+    json_data = dict(organization_id=organization_id, SECRET_KEY=config_instance.SECRET_KEY)
+    asyncio.run(__async_request(_url=_url, json_data=json_data, headers=headers))
 
 
 @cron_scheduler.scheduled_job(trigger='cron', day_of_week='mon-sun', hour=1)
@@ -16,10 +38,7 @@ def heroku_cron_affiliate_jobs() -> tuple:
         :return:
     ***REMOVED***
     _endpoint: str = '_cron/v1/affiliates'
-    _base_url: str = config_instance.BASE_URL
-    _url: str = f"{_base_url}{_endpoint}"
-
-    requests.get(url=_url)
+    send_request(_endpoint=_endpoint)
     return "OK", status_codes.status_ok_code
 
 
@@ -31,9 +50,7 @@ def heroku_cron_memberships() -> tuple:
         :return:
     ***REMOVED***
     _endpoint: str = '_cron/v1/memberships'
-    _base_url: str = config_instance.BASE_URL
-    _url: str = f"{_base_url}{_endpoint}"
-    requests.get(url=_url)
+    send_request(_endpoint=_endpoint)
     return "OK", status_codes.status_ok_code
 
 
@@ -44,9 +61,7 @@ def heroku_cron_transactions() -> tuple:
     :return:
     ***REMOVED***
     _endpoint: str = '_cron/v1/transactions'
-    _base_url: str = config_instance.BASE_URL
-    _url: str = f"{_base_url}{_endpoint}"
-    requests.get(url=_url)
+    send_request(_endpoint=_endpoint)
     return "OK", status_codes.status_ok_code
 
 
@@ -58,9 +73,7 @@ def heroku_cron_users() -> tuple:
         :return:
     ***REMOVED***
     _endpoint: str = '_cron/v1/users'
-    _base_url: str = config_instance.BASE_URL
-    _url: str = f"{_base_url}{_endpoint}"
-    requests.get(url=_url)
+    send_request(_endpoint=_endpoint)
     return "OK", status_codes.status_ok_code
 
 
