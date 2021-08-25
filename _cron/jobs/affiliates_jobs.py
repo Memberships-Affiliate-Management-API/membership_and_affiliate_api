3166a5c4c784360ae95dfbfda9b08c9f0f3149fc
mobius-crypt <mobiusndou@gmail.com>
2021-08-25 09:08:29 +0200

    updated: added affiliate cron jobs

---
+++ _cron/jobs/affiliates_jobs.py
@@ -0,0 +1,23 @@
+***REMOVED***
+        **affiliate_jobs**
+
+
+***REMOVED***
+
+
+class AffiliateCronTasks:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def finalize_affiliate_payments(self):
+        pass
+
+    def create_affiliate_reports(self):
+        pass
+
+
+
