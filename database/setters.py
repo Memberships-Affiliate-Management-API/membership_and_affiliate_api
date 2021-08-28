89f72456dcec3b1d0ab841451e96909acbd561fd
mobius-crypt <mobiusndou@gmail.com>
2021-08-28 11:44:51 +0200

    refactored setters.py

--- database/setters.py
+++ database/setters.py
@@ -11,7 +11,7 @@ __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affi
 __github_profile__ = "https://github.com/freelancing-solutions/"
 
 import typing
-from typing import List, Optional
+from typing import List
 from flask import escape
 from datetime import datetime, date
 from google.cloud import ndb
