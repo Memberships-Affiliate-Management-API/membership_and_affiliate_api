f6142612150a58ccd0a876b01f78011d20f2e1a0
mobius-crypt <mobiusndou@gmail.com>
2021-08-29 19:23:31 +0200

    created: api_requests.py

---
+++ config/api_requests.py
@@ -0,0 +1,28 @@
+***REMOVED***
+
+***REMOVED***
+
+
+class APIRequests:
+    ***REMOVED***
+        **Class APIRequests**
+    ***REMOVED***
+
+    def __init__(self, app):
+        ***REMOVED***
+
+        :param app:
+        ***REMOVED***
+        pass
+
+    def get(self, _endpoint, headers):
+        pass
+
+    def post(self, _endpoint, body, headers):
+        pass
+
+    def delete(self, _endpoint, headers):
+        pass
+
+    def put(self, _endpoint, body, headers):
+        pass
