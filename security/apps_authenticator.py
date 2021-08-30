c5398fc8c53b5f5a6cdf9d0f404ec10930230b05
mobius-crypt <mobiusndou@gmail.com>
2021-08-30 17:11:28 +0200

    updated app autheticator

--- security/apps_authenticator.py
+++ security/apps_authenticator.py
@@ -12,6 +12,7 @@ __twitter__ = "@blueitserver"
 __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
 __github_profile__ = "https://github.com/freelancing-solutions/"
 
+import hmac
 from typing import Optional
 import requests
 from flask import request
@@ -19,6 +20,7 @@ from config import config_instance
 from config.exceptions import UnAuthenticatedError, error_codes
 import functools
 from main import app_cache
+from database.app_authenticator import MicroAuthDetails
 
 
 @app_cache.memoize(timeout=16*60)
@@ -29,16 +31,24 @@ def is_app_authenticated(domain: str, secret_key: str, auth_token: str) -> bool:
     :param domain:
     :param secret_key:
     :param auth_token:
-    :return:
+    :return: True
     ***REMOVED***
     # TODO use system database to get details for authenticated applications
-    return True
+    compare_auth_token: bool = False
+    app_auth_details: MicroAuthDetails = MicroAuthDetails.query(MicroAuthDetails.domain == domain).get()
+    if isinstance(app_auth_details, MicroAuthDetails) and app_auth_details.domain == domain:
+        compare_auth_token: bool = hmac.compare_digest(auth_token, app_auth_details.auth_token)
+
+    compare_admin_domain: bool = hmac.compare_digest(domain, config_instance.ADMIN_APP_BASEURL)
+    compare_client_domain: bool = hmac.compare_digest(domain, config_instance.CLIENT_APP_BASEURL)
+    compare_secret_key: bool = hmac.compare_digest(secret_key, config_instance.SECRET_KEY)
+    return compare_secret_key and compare_client_domain and compare_admin_domain and compare_auth_token
 
 
 def handle_apps_authentication(func):
     @functools.wraps(func)
     def auth_wrapper(*args, **kwargs):
-        domain: Optional[str] = request.headers.get('domain')
+        domain: Optional[str] = request.headers.get('Referrer')
         secret_key: Optional[str] = kwargs.get('SECRET_KEY')
         auth_token: Optional[str] = kwargs.get('auth_token')
 
