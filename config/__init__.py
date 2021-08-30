1b61624c25c0a663b67b7b483f8f1e14dcc31a1f
mobius-crypt <mobiusndou@gmail.com>
2021-08-30 17:11:16 +0200

    refactored config details

--- config/__init__.py
+++ config/__init__.py
@@ -23,14 +23,26 @@ class Config:
     ***REMOVED***
     # TODO - Clean up configuration settings
     def __init__(self) -> None:
+        # APP URLS
         self.BASE_URL: str = os.environ.get("BASE_URL") or config("BASE_URL")
+        self.ADMIN_APP_BASEURL = os.environ.get("ADMIN_APP_BASEURL") or config("ADMIN_APP_BASEURL")
+        self.CLIENT_APP_BASEURL = os.environ.get("CLIENT_APP_BASEURL") or config("CLIENT_APP_BASEURL")
+
+        # MAILGUN Keys
         self.MAILGUN_DOMAIN: str = os.environ.get("MAILGUN_DOMAIN") or config("MAILGUN_DOMAIN")
         self.MAILGUN_API_KEY: str = os.environ.get("MAILGUN_API_KEY") or config("MAILGUN_API_KEY")
         self.MAILGUN_NO_RESPONSE: str = os.environ.get("MAILGUN_NO_RESPONSE") or config("MAILGUN_NO_RESPONSE")
         self.MAILGUN_VALIDATION_KEY: str = os.environ.get("MAILGUN_VALIDATION_KEY") or config("MAILGUN_VALIDATION_KEY")
         self.MAILGUN_WEBHOOK_KEY: str = os.environ.get("MAILGUN_WEBHOOK_KEY") or config("MAILGUN_WEBHOOK_KEY")
+
+        self.NO_RESPONSE_EMAIL: str = os.environ.get("NO_RESPONSE_EMAIL") or config("NO_RESPONSE_EMAIL")
+        self.SMTP_SERVER_URI: str = os.environ.get("SMTP_SERVER_URI") or config("SMTP_SERVER_URI")
+        self.SMTP_SERVER_PASSWORD: str = os.environ.get("SMTP_SERVER_PASSWORD") or config("SMTP_SERVER_PASSWORD")
+        self.SMTP_SERVER_USERNAME: str = os.environ.get("SMTP_SERVER_USERNAME") or config("SMTP_SERVER_USERNAME")
+
         self.PROJECT: str = os.environ.get("PROJECT") or config("PROJECT")
         self.APP_NAME: str = os.environ.get("APP_NAME") or config("APP_NAME")
+
         self.ORGANIZATION_ID: str = os.environ.get("ORGANIZATION_ID") or config("ORGANIZATION_ID")
         self.ADMIN_UID: str = os.environ.get("ADMIN_UID") or config("ADMIN_UID")
         self.DEFAULT_ACCESS_RIGHTS: typing.List[str] = ["visitor", "user", "super_user", "admin"]
@@ -39,35 +51,40 @@ class Config:
         self.ADMIN_SURNAME: str = os.environ.get("ADMIN_SURNAME") or config("ADMIN_SURNAME")
         self.ADMIN_PASSWORD: str = os.environ.get("ADMIN_PASSWORD") or config("ADMIN_PASSWORD")
         self.ADMIN_CELL: str = os.environ.get("ADMIN_CELL") or config("ADMIN_CELL")
-        self.NO_RESPONSE_EMAIL: str = os.environ.get("NO_RESPONSE_EMAIL") or config("NO_RESPONSE_EMAIL")
-        self.SMTP_SERVER_URI: str = os.environ.get("SMTP_SERVER_URI") or config("SMTP_SERVER_URI")
-        self.SMTP_SERVER_PASSWORD: str = os.environ.get("SMTP_SERVER_PASSWORD") or config("SMTP_SERVER_PASSWORD")
-        self.SMTP_SERVER_USERNAME: str = os.environ.get("SMTP_SERVER_USERNAME") or config("SMTP_SERVER_USERNAME")
-        self.UTC_OFFSET = datetime.timedelta(hours=2)
+
         self.PUBSUB_VERIFICATION_TOKEN = os.environ.get("PUBSUB_VERIFICATION_TOKEN") or config("PUBSUB_VERIFICATION_TOKEN")
+
+        self.UTC_OFFSET = datetime.timedelta(hours=2)
         self.DATASTORE_TIMEOUT: int = 360  # seconds 6 minutes
         self.DATASTORE_RETRIES: int = 3  # total retries when saving to datastore
+
         self.CURRENCY: str = "USD"
+
         # TODO obtain correct paypal client ids
         self.PAYPAL_CLIENT_ID: str = os.environ.get("PAYPAL_CLIENT_ID") or config("PAYPAL_CLIENT_ID")
         self.PAYPAL_CLIENT_SECRET: str = os.environ.get("PAYPAL_CLIENT_SECRET") or config("PAYPAL_CLIENT_SECRET")
         self.PAYPAL_CLIENT_ID_SAND: str = os.environ.get("PAYPAL_CLIENT_ID_SAND") or config("PAYPAL_CLIENT_ID_SAND")
         self.PAYPAL_CLIENT_SECRET_SAND: str = os.environ.get("PAYPAL_CLIENT_SECRET_SAND") or config("PAYPAL_CLIENT_SECRET_SAND")
+
+        self.GITHUB_CLIENT_ID: str = os.environ.get("GITHUB_CLIENT_ID") or config("GITHUB_CLIENT_ID")
+        self.GITHUB_CLIENT_SECRET: str = os.environ.get("GITHUB_CLIENT_SECRET") or config("GITHUB_CLIENT_SECRET")
+
         self.IS_PRODUCTION: bool = True
         self.SECRET_KEY: str = os.environ.get("SECRET_KEY") or config("SECRET_KEY")
         self.DEBUG: bool = False
+
         self.CACHE_TYPE: str = "simple"
         self.CACHE_DEFAULT_TIMEOUT: int = 60 * 60 * 6
         self.MEM_CACHE_SERVER_URI: str = ""
         self.GOOGLE_APPLICATION_CREDENTIALS: str = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") or config("GOOGLE_APPLICATION_CREDENTIALS")
+
         self.PAYMENT_PLANS_SCHEDULES: typing.List[str] = ['monthly', 'quarterly', 'annually']
         self.PAYMENT_PLANS_PAYMENT_DAYS: typing.List[int] = [1, 2, 3, 4, 5]
         self.MINIMUM_WITHDRAWAL_AMOUNT_USD: int = 3000  # amount is in cents
+
         self.ENV: str = "production"
         self.TEMPLATES_AUTO_RELOAD: bool = True
         self.PREFERRED_URL_SCHEME: str = "https"
-        self.GITHUB_CLIENT_ID: str = os.environ.get("GITHUB_CLIENT_ID") or config("GITHUB_CLIENT_ID")
-        self.GITHUB_CLIENT_SECRET: str = os.environ.get("GITHUB_CLIENT_SECRET") or config("GITHUB_CLIENT_SECRET")
 
         # NOTE : setting IS_PRODUCTION here - could find a better way of doing this rather
         # than depending on the OS
@@ -80,9 +97,6 @@ class Config:
             self.EXPLAIN_TEMPLATE_LOADING: bool = False
             self.PREFERRED_URL_SCHEME: str = "http"
             self.TESTING: bool = True
-            self.BASE_URL = 'http://localhost:8081/'
-            self.ADMIN_APP_BASEURL = 'http://localhost:8081/'
-            self.CLIENT_APP_BASEURL = 'http://localhost:8081/'
             # TODO - set Cache to MEM_CACHE and then setup the server URI, applicable on version 2
 
     def __str__(self) -> str:
