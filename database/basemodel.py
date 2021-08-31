5b2374406b5c3398fe4e18ffba0c394df3d387bb
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 21:47:32 +0200

    code refactored:  __docstr__

--- database/basemodel.py
+++ database/basemodel.py
@@ -2,11 +2,13 @@
     **ndb BaseModel **
         used as a superclass to define data models
 ***REMOVED***
+
 __developer__ = "mobius-crypt"
 __email__ = "mobiusndou@gmail.com"
 __twitter__ = "@blueitserver"
 __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
 __github_profile__ = "https://github.com/freelancing-solutions/"
+__licence__ = "MIT"
 
 from typing import Optional, List
 from google.cloud import ndb
