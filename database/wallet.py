738131a8c7147785942d296819c65973851e5838
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 21:48:28 +0200

    code refactored:  __docstr__

--- database/wallet.py
+++ database/wallet.py
@@ -2,11 +2,13 @@
     **Wallet Module**
     *Wallet Classes For Management of Clients / Organizations and Users Wallets"
 ***REMOVED***
+
 __developer__ = "mobius-crypt"
 __email__ = "mobiusndou@gmail.com"
 __twitter__ = "@blueitserver"
 __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
 __github_profile__ = "https://github.com/freelancing-solutions/"
+__licence__ = "MIT"
 
 import typing
 from datetime import datetime
