c791290874852c566ade45e73080a60e4de8ac00
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 21:47:41 +0200

    code refactored:  __docstr__

--- main/app/client/routes/home.py
+++ main/app/client/routes/home.py
@@ -2,11 +2,13 @@
     **Client Home Route**
         entry page to client dashboard
 ***REMOVED***
+
 __developer__ = "mobius-crypt"
 __email__ = "mobiusndou@gmail.com"
 __twitter__ = "@blueitserver"
 __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
 __github_profile__ = "https://github.com/freelancing-solutions/"
+__licence__ = "MIT"
 
 from typing import Optional
 
