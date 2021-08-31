8eadd5602ecbde330f09ea1229fe44e3f4062b2e
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 21:48:20 +0200

    code refactored:  __docstr__

--- database/setters.py
+++ database/setters.py
@@ -4,11 +4,13 @@
     the module will also trigger events when some database properties are being set or reset.
     allowing certain actions to be triggered if its necessary to do so.
 ***REMOVED***
+
 __developer__ = "mobius-crypt"
 __email__ = "mobiusndou@gmail.com"
 __twitter__ = "@blueitserver"
 __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
 __github_profile__ = "https://github.com/freelancing-solutions/"
+__licence__ = "MIT"
 
 import typing
 from typing import List
