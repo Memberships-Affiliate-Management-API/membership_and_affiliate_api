61b9393a9bf821e2b01d5d38661ac76c2d496070
mobius-crypt <mobiusndou@gmail.com>
2021-08-31 21:48:24 +0200

    code refactored:  __docstr__

--- database/users.py
+++ database/users.py
@@ -2,11 +2,13 @@
     **Users Database Module**
     *Class Definitions for Accessing And Management of Users and Clients Data**
 ***REMOVED***
+
 __developer__ = "mobius-crypt"
 __email__ = "mobiusndou@gmail.com"
 __twitter__ = "@blueitserver"
 __github_repo__ = "https://github.com/freelancing-solutions/memberships-and-affiliate-api"
 __github_profile__ = "https://github.com/freelancing-solutions/"
+__licence__ = "MIT"
 
 import typing
 from typing import Optional, List
