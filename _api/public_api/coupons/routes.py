4622bd9b73b5a77d57544ca7c6a80d43f03fc59e
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 22:04:57 +0200

    code refactored: views

--- _api/public_api/coupons/routes.py
+++ _api/public_api/coupons/routes.py
@@ -12,7 +12,7 @@ from typing import Optional
 from flask import Blueprint, request
 from config.exceptions import if_bad_request_raise
 from security.api_authenticator import handle_api_auth
-from views.memberships import CouponsView
+from views import coupons_view
 coupons_bp = Blueprint('coupons', __name__)
 
 
@@ -49,29 +49,28 @@ def coupons(path: str) -> tuple:
                 given organization_id return expired coupon codes
 
     ***REMOVED***
-    coupons_view_instance: CouponsView = CouponsView()
     # TODO - include organization_id for this routes, and refactor the view functions
     # Raises Bad Request error if request is not in json format
     if_bad_request_raise(request)
     coupon_data: dict = request.get_json()
 
     if path == "get":
-        return coupons_view_instance.get_coupon(coupon_data=coupon_data)
+        return coupons_view.get_coupon(coupon_data=coupon_data)
     elif path == "create":
-        return coupons_view_instance.add_coupon(coupon_data=coupon_data)
+        return coupons_view.add_coupon(coupon_data=coupon_data)
     elif path == "update":
-        return coupons_view_instance.update_coupon(coupon_data=coupon_data)
+        return coupons_view.update_coupon(coupon_data=coupon_data)
     elif path == "cancel":
-        return coupons_view_instance.cancel_coupon(coupon_data=coupon_data)
+        return coupons_view.cancel_coupon(coupon_data=coupon_data)
     elif path == "get-all":
         organization_id: Optional[str] = coupon_data.get('organization_id')
-        return coupons_view_instance.get_all_coupons(organization_id=organization_id)
+        return coupons_view.get_all_coupons(organization_id=organization_id)
     elif path == "get-valid":
         organization_id: Optional[str] = coupon_data.get('organization_id')
-        return coupons_view_instance.get_valid_coupons(organization_id=organization_id)
+        return coupons_view.get_valid_coupons(organization_id=organization_id)
     elif path == "get-expired":
         organization_id: Optional[str] = coupon_data.get('organization_id')
-        return coupons_view_instance.get_expired_coupons(organization_id=organization_id)
+        return coupons_view.get_expired_coupons(organization_id=organization_id)
     else:
         pass
     # TODO integrate Coupons to admin app and allow for the app to
