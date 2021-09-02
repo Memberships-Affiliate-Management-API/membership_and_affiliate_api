3b9162fa30d038b0f6bbf5e90c79e96553b66b63
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 22:05:46 +0200

    code refactored: views

--- views/__init__.py
+++ views/__init__.py
@@ -1,46 +1,22 @@
 ***REMOVED***
-    NOTES: configuring memcache in heroku
-# ... run command to enable cache
-heroku addons:create memcachier:dev
-
-from flask_caching import Cache
-
-cache = Cache()
-# ...
-
-def create_app():
-    app = Flask(__name__)
-
-    # ...
-
-    cache_servers = os.environ.get('MEMCACHIER_SERVERS')
-    if cache_servers == None:
-        cache.init_app(app, config={'CACHE_TYPE': 'simple'})
-    else:
-        cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-        cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-        cache.init_app(app,
-            config={'CACHE_TYPE': 'saslmemcached',
-                    'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-                    'CACHE_MEMCACHED_USERNAME': cache_user,
-                    'CACHE_MEMCACHED_PASSWORD': cache_pass,
-                    'CACHE_OPTIONS': { 'behaviors': {
-                        # Faster IO
-                        'tcp_nodelay': True,
-                        # Keep connection alive
-                        'tcp_keepalive': True,
-                        # Timeout for set/get requests
-                        'connect_timeout': 2000, # ms
-                        'send_timeout': 750 * 1000, # us
-                        'receive_timeout': 750 * 1000, # us
-                        '_poll_timeout': 2000, # ms
-                        # Better failover
-                        'ketama': True,
-                        'remove_failed': 1,
-                        'retry_timeout': 2,
-                        'dead_timeout': 30}}})
-
-  # ...
-
-  return app
+view instances declarations
 ***REMOVED***
+from views.affiliates import AffiliatesView, RecruitsView
+from views.apikeys import APIKeysView
+from views.memberships import MembershipsView, MembershipPlansView, CouponsView
+from views.organization import OrganizationView
+from views.services import ServicesView
+from views.users import UserView
+from views.wallet import WalletView
+
+
+affiliates_view: AffiliatesView = AffiliatesView()
+recruits_view: RecruitsView = RecruitsView()
+api_keys_view: APIKeysView = APIKeysView()
+memberships_view: MembershipsView = MembershipsView()
+membership_plans_view: MembershipPlansView = MembershipPlansView()
+coupons_view: CouponsView = CouponsView()
+organization_view: OrganizationView = OrganizationView()
+services_view: ServicesView = ServicesView()
+user_view: UserView = UserView()
+wallet_view: WalletView = WalletView()
