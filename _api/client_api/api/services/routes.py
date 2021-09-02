3218c8906145d93f110a16be4ce494eab9ce04d7
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 22:05:08 +0200

    code refactored: views

--- _api/client_api/api/services/routes.py
+++ _api/client_api/api/services/routes.py
@@ -11,7 +11,7 @@ __github_profile__ = "https://github.com/freelancing-solutions/"
 from typing import Optional
 from flask import Blueprint, request, current_app
 from config.exceptions import if_bad_request_raise, UnAuthenticatedError, error_codes
-from views.services import ServicesView
+from views import services_view
 
 services_client_api_bp = Blueprint('services_client_api', __name__)
 
@@ -27,7 +27,6 @@ def services_api(path: str) -> tuple:
     if_bad_request_raise(request)
     services_data: dict = request.get_json()
     secret_key: Optional[str] = services_data.get('SECRET_KEY')
-    services_view_instance: ServicesView = ServicesView()
 
     if not isinstance(secret_key, str) or secret_key != current_app.config.get('SECRET_KEY'):
         message: str = 'User Not Authorized: you cannot perform this action'
@@ -42,24 +41,24 @@ def services_api(path: str) -> tuple:
     home_url: Optional[str] = services_data.get('home_url')
 
     if path == "create-service":
-        return services_view_instance.create_service(organization_id=organization_id, uid=uid, name=name,
+        return services_view.create_service(organization_id=organization_id, uid=uid, name=name,
                                                      description=description, category=category, image_url=image_url,
                                                      home_url=home_url)
 
     elif path == "update-service":
         service_id: Optional[str] = services_data.get('service_id')
-        return services_view_instance.update_service(service_id=service_id, organization_id=organization_id, uid=uid,
+        return services_view.update_service(service_id=service_id, organization_id=organization_id, uid=uid,
                                                      name=name, description=description, category=category,
                                                      image_url=image_url, home_url=home_url)
 
     elif path == "activate-service":
         service_id: Optional[str] = services_data.get('service_id')
-        return services_view_instance.service_activation(service_id=service_id, organization_id=organization_id,
+        return services_view.service_activation(service_id=service_id, organization_id=organization_id,
                                                          uid=uid, is_active=True)
 
     elif path == "de-activate-service":
         service_id: Optional[str] = services_data.get('service_id')
-        return services_view_instance.service_activation(service_id=service_id, organization_id=organization_id,
+        return services_view.service_activation(service_id=service_id, organization_id=organization_id,
                                                          uid=uid, is_active=False)
 
 
