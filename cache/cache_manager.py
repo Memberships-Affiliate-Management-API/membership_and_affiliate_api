3f8a12d36b02fcfc3959dd2e8a8df6babb96da8b
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 21:18:36 +0200

    code refactor: cache_manager.py

--- cache/cache_manager.py
+++ cache/cache_manager.py
@@ -48,7 +48,6 @@ class CacheManager:
         ***REMOVED***
         self.cache.clear()
 
-    @staticmethod
     def _delete_user_cache(self, user_view, organization_id, uid, cell, email) -> bool:
         ***REMOVED***
             **deletes or invalidates invalid user cache
@@ -60,15 +59,14 @@ class CacheManager:
         ***REMOVED***
         # from views.users import UserView
         # TODO add delete for in-active cache items
-        self.delete_memoized(user_view.get_active_users, user_view, organization_id)
-        self.delete_memoized(user_view.get_active_users_async, user_view, organization_id)
-        self.delete_memoized(user_view.get_all_users, user_view, organization_id)
-        self.delete_memoized(user_view.get_all_users_async, user_view, organization_id)
-        self.delete_memoized(user_view.get_user, user_view, organization_id, uid, cell, email)
-        self.delete_memoized(user_view.get_user_async, user_view, organization_id, uid, cell, email)
+        self.cache.delete_memoized(user_view.get_active_users, user_view, organization_id)
+        self.cache.delete_memoized(user_view.get_active_users_async, user_view, organization_id)
+        self.cache.delete_memoized(user_view.get_all_users, user_view, organization_id)
+        self.cache.delete_memoized(user_view.get_all_users_async, user_view, organization_id)
+        self.cache.delete_memoized(user_view.get_user, user_view, organization_id, uid, cell, email)
+        self.cache.delete_memoized(user_view.get_user_async, user_view, organization_id, uid, cell, email)
         return True
 
-    @staticmethod
     def _delete_organization_cache(self, org_view, organization_id) -> bool:
         ***REMOVED***
             **_delete_organization_cache**
@@ -77,11 +75,10 @@ class CacheManager:
         :param organization_id: organization_id
         :return:
         ***REMOVED***
-        self.delete_memoized(org_view._return_all_organizations, org_view)
-        self.delete_memoized(org_view.get_organization, org_view, organization_id)
+        self.cache.delete_memoized(org_view._return_all_organizations, org_view)
+        self.cache.delete_memoized(org_view.get_organization, org_view, organization_id)
         return True
 
-    @staticmethod
     def _delete_wallet_cache(self, wallet_view, organization_id, uid) -> bool:
         ***REMOVED***
             **_delete_wallet_cache**
@@ -91,13 +88,12 @@ class CacheManager:
         :param uid:
         :return:
         ***REMOVED***
-        self.delete_memoized(wallet_view.get_wallet, wallet_view, organization_id, uid)
-        self.delete_memoized(wallet_view.get_wallet_async, wallet_view, organization_id, uid)
-        self.delete_memoized(wallet_view.return_all_wallets, wallet_view, organization_id)
-        self.delete_memoized(wallet_view.return_all_wallets_async, wallet_view, organization_id)
+        self.cache.delete_memoized(wallet_view.get_wallet, wallet_view, organization_id, uid)
+        self.cache.delete_memoized(wallet_view.get_wallet_async, wallet_view, organization_id, uid)
+        self.cache.delete_memoized(wallet_view.return_all_wallets, wallet_view, organization_id)
+        self.cache.delete_memoized(wallet_view.return_all_wallets_async, wallet_view, organization_id)
         return True
 
-    @staticmethod
     def _delete_membership_cache(self, membership_view, organization_id, uid, plan_id, status) -> bool:
         ***REMOVED***
             **_delete_membership_cache**
@@ -112,20 +108,18 @@ class CacheManager:
         # TODO- finish this up
         return True
 
-    @staticmethod
     def _delete_api_keys_cache(self, api_keys_view, api_key, organization_id) -> bool:
         ***REMOVED***
             **_delete_api_keys_cache**
                 delete stale api_keys cache instances
         :return:
         ***REMOVED***
-        self.delete_memoized(api_keys_view.return_all_organization_keys, api_keys_view, organization_id)
-        self.delete_memoized(api_keys_view.return_active_organization_keys, api_keys_view, organization_id)
-        self.delete_memoized(api_keys_view.get_api_key, api_keys_view, api_key, organization_id)
+        self.cache.delete_memoized(api_keys_view.return_all_organization_keys, api_keys_view, organization_id)
+        self.cache.delete_memoized(api_keys_view.return_active_organization_keys, api_keys_view, organization_id)
+        self.cache.delete_memoized(api_keys_view.get_api_key, api_keys_view, api_key, organization_id)
 
         return True
 
-    @staticmethod
     def _delete_affiliate_cache(self, affiliates_view, organization_id, affiliate_id):
         ***REMOVED***
             **_delete_affiliate_cache**
@@ -136,13 +130,12 @@ class CacheManager:
         :return:
         ***REMOVED***
         _dict: dict = dict(organization_id=organization_id, affiliate_id=affiliate_id)
-        self.delete_memoized(affiliates_view.get_affiliate, affiliates_view, _dict)
-        self.delete_memoized(affiliates_view.get_all_affiliates, affiliates_view, organization_id)
-        self.delete_memoized(affiliates_view.get_active_affiliates, affiliates_view, organization_id)
-        self.delete_memoized(affiliates_view.get_deleted_affiliates, affiliates_view, organization_id)
-        self.delete_memoized(affiliates_view.get_not_deleted_affiliates, affiliates_view, organization_id)
+        self.cache.delete_memoized(affiliates_view.get_affiliate, affiliates_view, _dict)
+        self.cache.delete_memoized(affiliates_view.get_all_affiliates, affiliates_view, organization_id)
+        self.cache.delete_memoized(affiliates_view.get_active_affiliates, affiliates_view, organization_id)
+        self.cache.delete_memoized(affiliates_view.get_deleted_affiliates, affiliates_view, organization_id)
+        self.cache.delete_memoized(affiliates_view.get_not_deleted_affiliates, affiliates_view, organization_id)
 
-    @staticmethod
     def _delete_recruits_cache(self, recruits_view, organization_id: str, is_active: Optional[bool] = None,
                                is_deleted: Optional[bool] = None, affiliate_data: Optional[dict] = None,
                                recruit_data: Optional[dict] = None) -> bool:
@@ -156,17 +149,16 @@ class CacheManager:
         :param recruit_data:
         :return:
         ***REMOVED***
-        self.delete_memoized(recruits_view.get_recruit, recruits_view, recruit_data)
-        self.delete_memoized(recruits_view.get_recruits_by_active_status, recruits_view,
-                             organization_id, is_active)
-        self.delete_memoized(recruits_view.get_recruits_by_deleted_status, recruits_view, organization_id,
-                             is_deleted)
-        self.delete_memoized(recruits_view.get_recruits_by_affiliate, recruits_view, affiliate_data)
-        self.delete_memoized(recruits_view.get_recruits_by_active_affiliate, recruits_view, affiliate_data,
-                             is_active)
+        self.cache.delete_memoized(recruits_view.get_recruit, recruits_view, recruit_data)
+        self.cache.delete_memoized(recruits_view.get_recruits_by_active_status, recruits_view,
+                                   organization_id, is_active)
+        self.cache.delete_memoized(recruits_view.get_recruits_by_deleted_status, recruits_view, organization_id,
+                                   is_deleted)
+        self.cache.delete_memoized(recruits_view.get_recruits_by_affiliate, recruits_view, affiliate_data)
+        self.cache.delete_memoized(recruits_view.get_recruits_by_active_affiliate, recruits_view, affiliate_data,
+                                   is_active)
         return True
 
-    @staticmethod
     def _delete_services_cache(self, services_view, organization_id: str, service_id: str) -> bool:
         ***REMOVED***
 
@@ -176,8 +168,8 @@ class CacheManager:
         :return:
         ***REMOVED***
 
-        self.delete_memoized(services_view.get_service, services_view, service_id, organization_id)
-        self.delete_memoized(services_view.return_services, services_view, organization_id)
+        self.cache.delete_memoized(services_view.get_service, services_view, service_id, organization_id)
+        self.cache.delete_memoized(services_view.return_services, services_view, organization_id)
         return True
 
     @staticmethod
