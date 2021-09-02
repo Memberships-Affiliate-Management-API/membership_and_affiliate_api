70778549aae296906ea80041dc435be9accf9b3a
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 22:05:36 +0200

    code refactored: views

--- _api/admin_api/api/apikeys/apikeys.py
+++ _api/admin_api/api/apikeys/apikeys.py
@@ -18,7 +18,7 @@ from flask import Blueprint, request, current_app
 from config import config_instance
 from config.exceptions import UnAuthenticatedError, error_codes
 from security.apps_authenticator import handle_apps_authentication
-from views.apikeys import APIKeysView
+from views import api_keys_view
 
 admin_api_keys_api_bp = Blueprint("admin_api_keys_api", __name__)
 
@@ -32,8 +32,6 @@ def api_keys(path: str) -> tuple:
     :param path:
     :return: results depending on path
     ***REMOVED***
-
-    api_keys_instance: APIKeysView = APIKeysView()
     json_data: dict = request.get_json()
     secret_key: Optional[str] = json_data.get('SECRET_KEY')
 
@@ -48,7 +46,7 @@ def api_keys(path: str) -> tuple:
         compare_uid: bool = hmac.compare_digest(uid, config_instance.ADMIN_UID)
         compare_org: bool = hmac.compare_digest(organization_id, config_instance.ORGANIZATION_ID)
         if compare_org and compare_uid:
-            return api_keys_instance.return_all_organization_keys(organization_id=organization_id)
+            return api_keys_view.return_all_organization_keys(organization_id=organization_id)
         message: str = 'User Not Authorized: you cannot perform this action'
         raise UnAuthenticatedError(status=error_codes.access_forbidden_error_code, description=message)
 
