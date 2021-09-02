848e218a1d05e18466ec3837942777d949b7f0c1
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 22:05:16 +0200

    code refactored: views

--- _api/client_api/api/memberships/routes.py
+++ _api/client_api/api/memberships/routes.py
@@ -14,7 +14,7 @@ from flask import Blueprint, request, current_app
 from config.exceptions import if_bad_request_raise, UnAuthenticatedError, error_codes
 from security.apps_authenticator import handle_apps_authentication
 from utils import today
-from views.memberships import MembershipsView, MembershipPlansView
+from views import memberships_view, membership_plans_view
 
 memberships_client_api_bp = Blueprint('memberships_client_api', __name__)
 
@@ -36,7 +36,6 @@ def memberships_client_api(path: str) -> tuple:
     ***REMOVED***
     if_bad_request_raise(request)
     json_data: dict = request.get_json()
-    memberships_view_instance: MembershipsView = MembershipsView()
 
     if isinstance(json_data, dict):
         secret_key: Optional[str] = json_data.get('SECRET_KEY')
@@ -55,15 +54,15 @@ def memberships_client_api(path: str) -> tuple:
             # consider verifying organization_id and uid
             payment_method: Optional[str] = json_data.get('payment_method')
             plan_start_date: date = today()
-            return memberships_view_instance.add_membership(organization_id=organization_id, uid=uid,
-                                                            plan_id=plan_id, payment_method=payment_method,
-                                                            plan_start_date=plan_start_date)
+            return memberships_view.add_membership(organization_id=organization_id, uid=uid,
+                                                   plan_id=plan_id, payment_method=payment_method,
+                                                   plan_start_date=plan_start_date)
 
         elif path == "un-subscribe":
             plan_id: Optional[str] = json_data.get('plan_id')
             uid: Optional[str] = json_data.get('uid')
             organization_id: Optional[str] = json_data.get('organization_id')
-            return memberships_view_instance.un_subscribe(organization_id=organization_id, uid=uid, plan_id=plan_id)
+            return memberships_view.un_subscribe(organization_id=organization_id, uid=uid, plan_id=plan_id)
 
 
 @memberships_client_api_bp.route('/_api/v1/client/admin/memberships/<string:path>', methods=["POST"])
@@ -83,7 +82,6 @@ def client_memberships_management(path: str) -> tuple:
     ***REMOVED***
     if_bad_request_raise(request)
     json_data: dict = request.get_json()
-    memberships_plan_view: MembershipPlansView = MembershipPlansView()
 
     # this endpoint allows users to create their own membership plan should be called after service
     # has been created and with the service or product id
@@ -113,22 +111,23 @@ def client_memberships_management(path: str) -> tuple:
 
         if path == "create-membership-plan":
             # NOTE: All monetary amounts are in cents - USD cents
-            return memberships_plan_view.add_plan(organization_id=organization_id, service_id=service_id,
+            return membership_plans_view.add_plan(organization_id=organization_id, service_id=service_id,
                                                   plan_name=plan_name, description=description,
                                                   schedule_day=schedule_day, schedule_term=schedule_term,
                                                   term_payment=term_payment, registration_amount=registration_amount,
                                                   currency=currency)
 
         elif path == "update-membership-plan":
-            return memberships_plan_view.update_plan(organization_id=organization_id, plan_id=plan_id,
-                                                     service_id=service_id, plan_name=plan_name, description=description,
+            return membership_plans_view.update_plan(organization_id=organization_id, plan_id=plan_id,
+                                                     service_id=service_id, plan_name=plan_name,
+                                                     description=description,
                                                      schedule_day=schedule_day, schedule_term=schedule_term,
                                                      term_payment=term_payment, registration_amount=registration_amount,
                                                      currency=currency, is_active=is_active)
 
         elif path == "get-plan":
-            return memberships_plan_view.return_plan(organization_id=organization_id, plan_id=plan_id)
+            return membership_plans_view.return_plan(organization_id=organization_id, plan_id=plan_id)
 
         elif path == "return-all-plans":
             # NOTE this will return all main membership plans for subscribing to this API
-            return memberships_plan_view.return_all_plans(organization_id=organization_id)
+            return membership_plans_view.return_all_plans(organization_id=organization_id)
