53943c3676cd01c493413d57d17747d553fb18b9
mobius-crypt <mobiusndou@gmail.com>
2021-09-02 22:05:55 +0200

    code refactored: views

---
+++ cache/__init__.py
@@ -0,0 +1,46 @@
+***REMOVED***
+    NOTES: configuring memcache in heroku
+# ... run command to enable cache
+heroku addons:create memcachier:dev
+
+from flask_caching import Cache
+
+cache = Cache()
+# ...
+
+def create_app():
+    app = Flask(__name__)
+
+    # ...
+
+    cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+    if cache_servers == None:
+        cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+    else:
+        cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+        cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+        cache.init_app(app,
+            config={'CACHE_TYPE': 'saslmemcached',
+                    'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+                    'CACHE_MEMCACHED_USERNAME': cache_user,
+                    'CACHE_MEMCACHED_PASSWORD': cache_pass,
+                    'CACHE_OPTIONS': { 'behaviors': {
+                        # Faster IO
+                        'tcp_nodelay': True,
+                        # Keep connection alive
+                        'tcp_keepalive': True,
+                        # Timeout for set/get requests
+                        'connect_timeout': 2000, # ms
+                        'send_timeout': 750 * 1000, # us
+                        'receive_timeout': 750 * 1000, # us
+                        '_poll_timeout': 2000, # ms
+                        # Better failover
+                        'ketama': True,
+                        'remove_failed': 1,
+                        'retry_timeout': 2,
+                        'dead_timeout': 30}}})
+
+  # ...
+
+  return app
+***REMOVED***
