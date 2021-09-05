b826014482a27ec830512a7cafe10f1994b0ed88
mobius-crypt <mobiusndou@gmail.com>
2021-09-05 16:45:29 +0200

    updated: OrganizationQueryMock

--- tests/test_views/test_organizations.py
+++ tests/test_views/test_organizations.py
@@ -18,3 +18,19 @@ from utils import create_id, timestamp, today, _char_set
 with test_app().app_context():
     from views import organization_view
 
+
+class OrganizationQueryMock:
+    organization_instance: Organization = Organization()
+
+    def __init__(self):
+        self.organization_instance.organization_id = create_id()
+        self.organization_instance.organization_name = "Memberships & Affiliates API"
+        self.organization_instance.description = "main organization for Memberships & Affiliates API"
+        self.organization_instance.wallet_id = create_id()
+        self.organization_instance.owner_uid = create_id()
+        self.organization_instance.total_affiliates = randint(10, 1000)
+        self.organization_instance.total_members = randint(10, 1000)
+        self.organization_instance.total_users = self.organization_instance.total_members + randint(10, 1000)
+        self.organization_instance.home_url = 'https://memberships-affiliates-api.heroku.com'
+        self.organization_instance.login_callback_url = 'https://memberships-affiliates-api.heroku.com/login'
+        self.organization_instance.recovery_callback_url = 'https://memberships-affiliates-api.heroku.com/recovery'
