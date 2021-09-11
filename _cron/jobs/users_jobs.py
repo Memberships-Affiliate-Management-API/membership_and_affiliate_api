4f020092791fcfd3189ea84d0d33e987d916f3ab
mobius-crypt <mobiusndou@gmail.com>
2021-09-11 13:29:36 +0200

    fixed context handling on users

--- _cron/jobs/users_jobs.py
+++ _cron/jobs/users_jobs.py
@@ -5,7 +5,11 @@
 """
 import asyncio
 from typing import List, Coroutine, Optional
+
+from google.cloud.ndb import toplevel, tasklet, Future, wait_all
+
 from _sdk._email import Mailgun
+from config.use_context import use_context
 from database.users import UserModel
 from utils import today, date_days_ago
 
@@ -20,10 +24,13 @@ class UserJobs(Mailgun):
     def __init__(self):
         super(UserJobs, self).__init__()
 
+    @use_context
+    @toplevel
     def run(self) -> None:
-        asyncio.run(self.send_login_reminders())
+        login_reminders = yield self.send_login_reminders()
 
-    async def do_send_login_reminder(self, user_instance: UserModel):
+    @tasklet
+    def do_send_login_reminder(self, user_instance: UserModel):
         """
             **do_send_login_reminder**
                 send actual login reminder to user
@@ -33,7 +40,7 @@ class UserJobs(Mailgun):
         """
         uid: str = user_instance.uid
         organization_id: str = user_instance.organization_id
-        org_data: Optional[dict] = await self._get_organization_data_async(organization_id=organization_id, uid=uid)
+        org_data: Optional[dict] = asyncio.run(self._get_organization_data_async(organization_id=organization_id, uid=uid))
         subject: str = f"{org_data.get('organization_name')} We missed you"
         text: str = f'''
         Hi {user_instance.names} {user_instance.surname}
@@ -57,18 +64,15 @@ class UserJobs(Mailgun):
         '''
         self._do_schedule_mail(to_email=user_instance.email, subject=subject, text=text, html=html)
 
-    async def send_login_reminders(self) -> None:
+    @tasklet
+    def send_login_reminders(self) -> List[Future]:
         """
         **send_login_reminders**
             sends login reminders to users and developers
         :return: None
         """
         seven_days_ago = date_days_ago(days=7)
-        users_list: List[UserModel] = [user for user in UserModel.query(UserModel.email_verified == True).fetch_async()
+        users_list: List[UserModel] = [user for user in UserModel.query(UserModel.email_verified == True).fetch_async().get_result()
                                        if user.last_login_date < seven_days_ago]
 
-        cron_jobs: List[Coroutine] = [self.do_send_login_reminder(user_instance=user) for user in users_list]
-        event_loop = asyncio.get_event_loop()
-        event_loop.run_until_complete(asyncio.gather(cron_jobs))
-        event_loop.close()
-
+        return [self.do_send_login_reminder(user_instance=user) for user in users_list]
