3e45c750e7a2f103fe9afa325bb5537a4ec347f5
mobius-crypt <mobiusndou@gmail.com>
2021-09-17 14:21:49 +0200

    updated: added helpdesk test cases

--- tests/test_views/test_helpdesk.py
+++ tests/test_views/test_helpdesk.py
@@ -11,7 +11,7 @@ from typing import List
 from google.cloud import ndb
 from config.exceptions import status_codes, InputError, UnAuthenticatedError, DataServiceError
 from config.use_context import get_client
-from database.affiliates import Affiliates
+from database.helpdesk import HelpDesk, Ticket
 from tests import test_app
 from utils import create_id
 from config import config_instance
@@ -20,3 +20,17 @@ with test_app().app_context():
     from views import helpdesk_view, ticket_view
 
 
+class HelpDeskQueryMock:
+    helpdesk_instance: HelpDesk = HelpDesk()
+
+    def __init__(self) -> None:
+        self.helpdesk_instance.help_desk_active = True
+        self.helpdesk_instance.total_tickets = 0
+        self.helpdesk_instance.total_tickets_opened = 0
+        self.helpdesk_instance.total_tickets_closed = 0
+
+    def get(self) -> HelpDesk:
+        return self.helpdesk_instance
+
+    def fetch(self) -> List[HelpDesk]:
+        pass
