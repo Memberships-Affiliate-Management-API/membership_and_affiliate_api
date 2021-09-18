520ead9a20644f60a9551aebf0214038de151960
mobius-crypt <mobiusndou@gmail.com>
2021-09-18 11:27:11 +0200

    code refactored: scheduler.py tasks.py

--- tasks.py
+++ tasks.py
@@ -1,12 +1,29 @@
 """
     will start task scheduler as a service
 """
+from threading import Thread
+from typing import Optional, List
+
 from config.use_context import get_client
 from schedulers.scheduler import task_scheduler
 
 
-def run_tasks():
+def start_task() -> None:
     print(f'running tasks...')
     task_scheduler.run_all(delay_seconds=5)
     task_scheduler.clear()
     print('done running tasks')
+    return None
+
+
+class TasksRunManager:
+    def __init__(self):
+        self.tasks_list: List[Optional[Thread]] = []
+
+    def add_tasks(self, task: Thread):
+        if not len(self.tasks_list):
+            self.tasks_list.append(task.start())
+
+        for idx, tasks in enumerate(self.tasks_list):
+            if tasks is None:
+                self.tasks_list[idx] = task.start()
