8863ba6f6ac33deec293ad0755673477366ea09b
mobius-crypt <mobiusndou@gmail.com>
2021-10-02 15:33:02 +0200

    mark_affiliate as active or in-active

--- _api/admin_api/api/affiliates/affiliates.py
+++ _api/admin_api/api/affiliates/affiliates.py
@@ -16,7 +16,7 @@ from typing import Optional
 from flask import Blueprint, request
 
 from config import config_instance
-from config.exceptions import UnAuthenticatedError
+from config.exceptions import UnAuthenticatedError, InputError
 from security.apps_authenticator import handle_apps_authentication, verify_secret_key
 from views import affiliates_view
 
@@ -47,3 +47,45 @@ def admin_affiliates(path: str) -> tuple:
             return affiliates_view.get_all_affiliates(organization_id=organization_id)
         message: str = "You are not authorized to access this resource"
         raise UnAuthenticatedError(description=message)
+    elif path == "register-affiliate":
+        __doc__ = """create a new memberships and admin affiliate"""
+        return affiliates_view.register_affiliate(affiliate_data=get_affiliate_data(json_data))
+    elif path == "update-total-recruits":
+        __doc__ = """update total number of recruits for memberships and admin affiliate"""
+        affiliate_data: dict = get_affiliate_data(json_data)
+        try:
+            add: int = int(json_data.get('add'))
+        except ValueError:
+            _message: str = 'add must be an integer'
+            raise InputError(description=_message)
+        return affiliates_view.total_recruits(affiliate_data=affiliate_data, add=add)
+    elif path == "delete-affiliate":
+        __doc__ = """delete affiliate"""
+        return affiliates_view.delete_affiliate(affiliate_data=get_affiliate_data(json_data))
+    elif path == "mark-active":
+        affiliate_data: dict = get_affiliate_data(json_data)
+        is_active: bool = json_data.get('is_active')
+        return affiliates_view.mark_active(affiliate_data=affiliate_data, is_active=is_active)
+
+
+def get_affiliate_data(json_data: dict) -> dict:
+    """
+        **get_affiliate_data**
+            given json_data as input validate data and return affiliate_data
+    :param json_data:
+    :raises: InputError and UnAuthenticatedError
+    :return: affiliate_data as dict
+    """
+    organization_id: str = json_data.get('organization_id')
+    uid: str = json_data.get('uid')
+    compare_organization: bool = hmac.compare_digest(organization_id, config_instance.ORGANIZATION_ID)
+    compare_uid: bool = hmac.compare_digest(uid, config_instance.ADMIN_UID)
+    if not (compare_uid and compare_organization):
+        message: str = "You are not authorized to access this resource"
+        raise UnAuthenticatedError(description=message)
+    affiliate_data: dict = json_data.get('affiliate_data')
+    if isinstance(affiliate_data, dict):
+        _message: str = 'please provide affiliate data in order to register affiliate'
+        raise InputError(description=_message)
+    return affiliate_data
+
