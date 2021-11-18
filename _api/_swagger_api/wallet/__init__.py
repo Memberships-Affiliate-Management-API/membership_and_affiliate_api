baad2509ba006bcb5f5fb457d79333b8fc8c7446
mobius-crypt <mobiusndou@gmail.com>
2021-11-18 19:49:40 +0200

    updated requests and schemas for WalletView

--- _api/_swagger_api/wallet/__init__.py
+++ _api/_swagger_api/wallet/__init__.py
@@ -1,21 +1,44 @@
 """
     Swagger Compatible API for wallet
 """
+from datetime import datetime
+
+from flask_apispec import doc
+
 from _api._swagger_api import ViewModel
 from security.api_authenticator import handle_api_auth
 from flask_restful import fields, marshal_with
+from marshmallow import Schema, fields
 from views import wallet_view
 from database.wallet import WalletModel
 
 
+class AmountSchema(Schema):
+    amount_cents = fields.Integer(default=0)
+    currency: str = fields.String()
+
+
 # Wallet Payload Schema
-WalletPayloadSchema = dict(**WalletModel().to_dict())
+class WalletPayloadSchema(Schema):
+    organization_id: str = fields.String()
+    uid: str = fields.String()
+    wallet_id: str = fields.String()
+    is_org_wallet: bool = fields.Boolean(default=False)
+    available_funds = fields.Nested(AmountSchema)
+    monthly_withdrawal_allowance = fields.Nested(AmountSchema)
+    time_created: datetime = fields.DateTime()
+    last_transaction_time: datetime = fields.DateTime()
+    paypal_address: str = fields.String()
+    is_verified: bool = fields.String()
+
 
 # Wallet Response Schema
 WalletResponseSchema = dict(
     status=fields.Boolean(default=False),
     message=fields.String(),
     payload=fields.Nested(WalletPayloadSchema))
+
+# TODO add request Schema
 
 
 class WalletView(ViewModel):
@@ -30,7 +53,9 @@ class WalletView(ViewModel):
         super().__init__()
 
     @staticmethod
-    def get(self, organization_id: str, uid: str) -> tuple:
+    @doc(description=wallet_view.get_wallet.__doc__)
+    @marshal_with(WalletResponseSchema)
+    def get(organization_id: str, uid: str) -> tuple:
         """
             eval(wallet_view.get_wallet.__doc__)
         :return:
@@ -38,7 +63,9 @@ class WalletView(ViewModel):
         return wallet_view.get_wallet(organization_id=organization_id, uid=uid)
 
     @staticmethod
-    def put(self, wallet_dict: dict) -> tuple:
+    @doc(description=wallet_view.update_wallet.__doc__)
+    @marshal_with(WalletResponseSchema)
+    def put(wallet_dict: dict) -> tuple:
         """
             updates an existing wallet by uid
         :return:
@@ -46,7 +73,9 @@ class WalletView(ViewModel):
         return wallet_view.update_wallet(wallet_data=wallet_dict)
 
     @staticmethod
-    def delete(self, organization_id: str, uid: str) -> tuple:
+    @doc(description=wallet_view.reset_wallet.__doc__)
+    @marshal_with(WalletResponseSchema)
+    def delete(organization_id: str, uid: str) -> tuple:
         """
             deletes an existing wallet by uid
         :return:
