7afacc0f6d5040836cb30b56c73dd112e3c2a56d
mobius-crypt <mobiusndou@gmail.com>
2021-11-19 19:10:15 +0200

    added schemas

---
+++ _api/_swagger_api/schemas/wallet.py
@@ -0,0 +1,42 @@
+from flask_restful import fields, marshal_with
+from marshmallow import Schema, fields
+
+
+
+
+class AmountSchema(Schema):
+    """
+        ** Class AmountSchema **
+
+    """
+    amount_cents = fields.Integer(default=0)
+    currency: str = fields.String()
+
+
+# Wallet Payload Schema
+class WalletPayloadSchema(Schema):
+    """
+        **Class WalletPayloadSchema**
+            a schema for payload
+    """
+    organization_id: str = fields.String()
+    uid: str = fields.String()
+    wallet_id: str = fields.String()
+    is_org_wallet: bool = fields.Boolean(default=False)
+    available_funds = fields.Nested(AmountSchema)
+    monthly_withdrawal_allowance = fields.Nested(AmountSchema)
+    time_created: datetime = fields.DateTime()
+    last_transaction_time: datetime = fields.DateTime()
+    paypal_address: str = fields.String()
+    is_verified: bool = fields.String()
+
+
+# Wallet Response Schema
+class WalletResponseSchema(Schema):
+    """
+        **WalletResponseSchema**
+            a schema for wallet responses
+    """
+    status = fields.Boolean(default=False)
+    message = fields.String()
+    payload = fields.Nested(WalletPayloadSchema)
--- _api/_swagger_api/wallet/__init__.py
+++ _api/_swagger_api/wallet/__init__.py
@@ -2,53 +2,13 @@
     Swagger Compatible API for wallet
 """
 from datetime import datetime
-
 from flask_apispec import doc
-
 from _api._swagger_api import ViewModel
 from security.api_authenticator import handle_api_auth
 from flask_restful import fields, marshal_with
 from marshmallow import Schema, fields
 from views import wallet_view
 from database.wallet import WalletModel
-
-
-class AmountSchema(Schema):
-    """
-        ** Class AmountSchema **
-
-    """
-    amount_cents = fields.Integer(default=0)
-    currency: str = fields.String()
-
-
-# Wallet Payload Schema
-class WalletPayloadSchema(Schema):
-    """
-        **Class WalletPayloadSchema**
-            a schema for payload
-    """
-    organization_id: str = fields.String()
-    uid: str = fields.String()
-    wallet_id: str = fields.String()
-    is_org_wallet: bool = fields.Boolean(default=False)
-    available_funds = fields.Nested(AmountSchema)
-    monthly_withdrawal_allowance = fields.Nested(AmountSchema)
-    time_created: datetime = fields.DateTime()
-    last_transaction_time: datetime = fields.DateTime()
-    paypal_address: str = fields.String()
-    is_verified: bool = fields.String()
-
-
-# Wallet Response Schema
-class WalletResponseSchema(Schema):
-    """
-        **WalletResponseSchema**
-            a schema for wallet responses
-    """
-    status = fields.Boolean(default=False)
-    message = fields.String()
-    payload = fields.Nested(WalletPayloadSchema)
 
 
 # TODO add request Schema
