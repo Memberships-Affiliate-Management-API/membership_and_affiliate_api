30c8eb006559cf57f1587b17a23748f79b417f22
mobius-crypt <mobiusndou@gmail.com>
2021-11-19 20:51:46 +0200

    added class for creating organizational wallets

--- _api/_swagger_api/wallet/__init__.py
+++ _api/_swagger_api/wallet/__init__.py
@@ -18,7 +18,7 @@ class WalletView(ViewModel):
         **WalletView**
             get, update, delete Wallet
     """
-    methods = ["GET", "PUT", "DELETE"]
+    methods = ["GET", "PUT", "POST", "DELETE"]
     method_decorators = [handle_api_auth]
 
     def __init__(self) -> None:
@@ -43,6 +43,26 @@ class WalletView(ViewModel):
         :return:
         """
         return wallet_view.update_wallet(wallet_data=wallet_dict)
+
+    @staticmethod
+    @doc(description=wallet_view.create_wallet.__doc__)
+    @marshal_with(WalletResponseSchema)
+    def post(wallet_dict: dict) -> tuple:
+        """
+        **Create User Wallet**
+            create new user wallet
+        :return:
+        """
+        organization_id: str = wallet_dict.get('organization_id')
+        uid: str = wallet_dict.get('uid')
+        currency: str = wallet_dict.get('currency')
+        paypal_address: str = wallet_dict.get('paypal_address')
+
+        return wallet_view.create_wallet(organization_id=organization_id,
+                                         uid=uid,
+                                         currency=currency,
+                                         paypal_address=paypal_address,
+                                         is_org_wallet=False)
 
     @staticmethod
     @doc(description=wallet_view.reset_wallet.__doc__)
@@ -76,3 +96,32 @@ class WalletListView(ViewModel):
         :return:
         """
         return wallet_view.return_all_wallets(organization_id=organization_id)
+
+
+class OrganizationWallets(ViewModel):
+    """
+        **OrganizationWallets**
+            create organization wallets
+    """
+    methods = ["POST"]
+    method_decorators = [handle_api_auth]
+
+    @staticmethod
+    @doc(description=wallet_view.create_wallet.__doc__)
+    @marshal_with(WalletResponseSchema)
+    def post(wallet_dict: dict) -> tuple:
+        """
+        **Create Organization Wallet**
+            create a new organizational wallet
+        :return:
+        """
+        organization_id: str = wallet_dict.get('organization_id')
+        uid: str = wallet_dict.get('uid')
+        currency: str = wallet_dict.get('currency')
+        paypal_address: str = wallet_dict.get('paypal_address')
+
+        return wallet_view.create_wallet(organization_id=organization_id,
+                                         uid=uid,
+                                         currency=currency,
+                                         paypal_address=paypal_address,
+                                         is_org_wallet=True)
