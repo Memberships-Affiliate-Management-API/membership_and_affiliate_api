b4623b73c2ae52f2a9d502f6d5b3a1d7e262c6ee
mobius-crypt <mobiusndou@gmail.com>
2021-11-20 15:18:07 +0200

    updated Schemas to include valid description of list in Schemas

--- _swagger_api/schemas/wallet.py
+++ _swagger_api/schemas/wallet.py
@@ -2,6 +2,7 @@
     **Wallet Schemas**
 """
 import required
+from lxml.html._diffcommand import description
 from marshmallow import Schema, fields
 from datetime import datetime
 from _swagger_api.schemas.amount import AmountSchema
@@ -14,15 +15,15 @@ class WalletPayloadSchema(Schema):
             a schema for payload
     """
     organization_id: str = fields.String(required=True, description='organization id of the registered organization')
-    uid: str = fields.String(required=True)
-    wallet_id: str = fields.String(required=True)
-    is_org_wallet: bool = fields.Boolean(default=False)
-    available_funds = fields.Nested(AmountSchema)
-    monthly_withdrawal_allowance = fields.Nested(AmountSchema)
-    time_created: datetime = fields.DateTime()
-    last_transaction_time: datetime = fields.DateTime()
-    paypal_address: str = fields.String()
-    is_verified: bool = fields.String()
+    uid: str = fields.String(required=True, description='user id of the user making the request')
+    wallet_id: str = fields.String(required=True, description='wallet_id the id of the wallet')
+    is_org_wallet: bool = fields.Boolean(default=False, description='True if this wallet is an organization wallet')
+    available_funds = fields.Nested(AmountSchema, description='indicates funds available in the wallet')
+    monthly_withdrawal_allowance = fields.Nested(AmountSchema, description='indicates funds available for withdrawal')
+    time_created: datetime = fields.DateTime(description='the date and time the wallet was created')
+    last_transaction_time: datetime = fields.DateTime(description='the date the last transaction was performed')
+    paypal_address: str = fields.String(description='paypal address attached to wallet - withdrawals will be sent to this address')
+    is_verified: bool = fields.String(description='True if wallet is verified, only verified wallets can receive withdrawals')
 
 
 # Wallet Response Schema
