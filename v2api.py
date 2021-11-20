d94b0947de9c3ed1c9d74e86a5b56045a2c4a69b
mobius-crypt <mobiusndou@gmail.com>
2021-11-20 14:49:26 +0200

    finalizing version 2 api

--- v2api.py
+++ v2api.py
@@ -1,3 +1,10 @@
+from flask_restful import Api
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+
+docs = FlaskApiSpec()
+
 
 def register_v2_api(app):
     """
@@ -5,5 +12,23 @@ def register_v2_api(app):
     :param app:
     :return:
     """
+    from _swagger_api.users import UserViewModel, UserListView, AuthViewModel
+    api = Api(app)
+    api.add_resource(UserViewModel, '/api/v2/user', endpoint='create_user', methods=['POST'])
+
+    app.config.update({
+        'APISPEC_SPEC': APISpec(
+            title='Memberships & Affiliate Management API',
+            version='0.0.1',
+            plugins=[MarshmallowPlugin()],
+            openapi_version='2.0.0'
+        ),
+        'APISPEC_SWAGGER_URL': '/api',
+        'APISPEC_SWAGGER_UI_URL': '/api-ui'
+
+    })
+    # registering documentation
+    docs.init_app(app)
+    docs.register(target=UserViewModel, endpoint='create_user')
     return app
 
