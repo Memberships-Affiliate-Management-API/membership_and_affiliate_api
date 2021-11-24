23f45ff9fec1eddbb89e065d8aa2c5b56f0fe5f6
mobius-crypt <mobiusndou@gmail.com>
2021-11-24 17:41:18 +0200

    adding memberships view

---
+++ _swagger_api/memberships/__init__.py
@@ -0,0 +1,42 @@
+from _swagger_api import ViewModel
+
+
+class MembershipsView(ViewModel):
+    """
+    View model for Memberships
+    """
+    methods = ['GET', 'POST', 'PUT', 'DELETE']
+
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def get(self,**paylod):
+        """
+        Get all memberships
+        """
+        pass
+    
+
+
+    @staticmethod
+    def post(self, **payload):
+        """
+        Create a new membership
+        """
+        pass
+
+    @staticmethod
+    def put(self, **payload):
+        """
+        Update a membership
+        """
+        pass
+
+    @staticmethod
+    def delete(self, **payload):
+        """
+        Delete a membership
+        """
+        pass
+    
