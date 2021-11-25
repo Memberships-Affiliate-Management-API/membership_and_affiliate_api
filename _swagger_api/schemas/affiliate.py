584970223715968d3fff03abe4b75ffdd9877430
mobius-crypt <mobiusndou@gmail.com>
2021-11-25 19:46:33 +0200

    added Affiliate Payload Schemas

--- _swagger_api/schemas/affiliate.py
+++ _swagger_api/schemas/affiliate.py
@@ -1,9 +1,35 @@
+from datetime import datetime
 from marshmallow import Schema, fields
+
+from _swagger_api.schemas.response import ResponseSchema
 
 
 class AffiliatePayloadSchema(Schema):
     """
         **Class AffiliatePayloadSchema**
     """
-    pass
+    organization_id: str = fields.String()
+    affiliate_id: str = fields.String()
+    uid: str = fields.String()
+    last_updated: datetime = fields.DateTime()
+    datetime_recruited: datetime = fields.DateTime()
+    total_recruited: int = fields.Integer(default=0)
+    is_active: bool = fields.Boolean(default=True)
+    is_deleted: bool = fields.Boolean(default=False)
+
+
+class AffiliateResponseSchema(ResponseSchema):
+    """
+        **Class AffiliateResponseSchema**
+    """
+    payload = fields.Nested(AffiliatePayloadSchema)
+
+
+class AffiliateListResponseSchema(ResponseSchema):
+    """
+        **Class AffiliateListResponseSchema**
+    """
+    payload = fields.Nested(AffiliatePayloadSchema(many=True))
+
+
 
