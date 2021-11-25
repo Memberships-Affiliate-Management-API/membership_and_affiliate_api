81c6c3db023a53a6d1f2c07bf6d4c84de9486dfa
mobius-crypt <mobiusndou@gmail.com>
2021-11-25 19:50:57 +0200

    added Affiliate Payload Schemas

---
+++ _swagger_api/affiliate/__init__.py
@@ -0,0 +1,17 @@
+from _swagger_api import ViewModel
+
+
+class AffiliateView(ViewModel):
+    """
+
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+    @staticmethod
+    def get():
+        """
+
+        :return:
+        """
+        pass
