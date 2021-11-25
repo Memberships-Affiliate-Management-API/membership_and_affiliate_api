ac07ba338b7ac6f8c9b86f135c9b9219f807e415
mobius-crypt <mobiusndou@gmail.com>
2021-11-25 20:10:02 +0200

    added get method on affiliateView

--- _swagger_api/affiliate/__init__.py
+++ _swagger_api/affiliate/__init__.py
@@ -1,17 +1,28 @@
+from flask_apispec import doc, marshal_with, use_kwargs
+
 from _swagger_api import ViewModel
+from _swagger_api.schemas.affiliate import AffiliateResponseSchema
+from security.api_authenticator import handle_api_auth
+from views import affiliates_view
 
 
 class AffiliateView(ViewModel):
     """
+        **Class AffiliateView**
 
     """
+    methods = ['GET', 'POST', 'PUT', 'GET']
+    method_decorators = [handle_api_auth]
+
     def __init__(self) -> None:
         super().__init__()
 
     @staticmethod
-    def get():
+    @doc(description=affiliates_view.get_affiliate.__doc__)
+    @marshal_with(AffiliateResponseSchema)
+    def get(**payload) -> tuple:
         """
-
-        :return:
+        ** obtains a specific affiliate record **
+            :return:
         """
-        pass
+        return affiliates_view.get_affiliate(payload)
