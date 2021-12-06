7ec69e30606a7b0d2233778920c4027bae27149b
mobius-crypt <mobiusndou@gmail.com>
2021-12-06 07:52:45 +0200

    updated Coupons Schema

--- _swagger_api/schemas/coupons.py
+++ _swagger_api/schemas/coupons.py
@@ -1,5 +1,32 @@
+"""
+    Coupons schema
+"""
 from marshmallow import Schema, fields
 
 
-class CouponPayloadSchema(Schema):
-    pass
+class PayloadSchema(Schema):
+    """
+    ** Class CouponPayloadSchema **
+        Coupon schema
+
+    """
+    organization_id = fields.Integer(required=True)
+    code = fields.String(required=True)
+    discount_percent = fields.Float()
+    is_valid = fields.Boolean(default=True)
+    date_created = fields.DateTime(required=True)
+    expiration_time = fields.Integer(required=True)
+
+class CouponResponseSchema(ResponseSchema):
+    """
+        Coupon schema
+    """
+    payload = fields.Nested(PayloadSchema)
+
+
+
+class CouponResponseListSchema(ResponseSchema):
+    """
+        Coupon schema
+    """
+    payload = fields.Nested(PayloadSchema, many=True)
